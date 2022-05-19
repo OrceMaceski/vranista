5eadce8b53970a9d5b4933b7a3cb747baca5f7ca
orcemaceski <orce@maceski.net>
2022-05-19 15:24:27 +0200

    Contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,22 @@ class PagesController < ApplicationController
 
   def about; end
 
-  def contact; end
+  def contact
+  end
+  @contact = Contact.new
+
+  def create_contact
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path, notice: 'Message sent!'
+    else
+      render :contact, alert: 'Error sending message.'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
