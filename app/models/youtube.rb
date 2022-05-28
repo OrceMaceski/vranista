69de562af358922a7ccb9a055720ab4d0884baa6
orcemaceski <orce@kennedytech.mk>
2022-05-28 18:00:29 +0200

    Convert to embeded video when saving

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Youtube < ApplicationRecord
-    validates :link, presence: true
+  before_save :convert_to_embeded
+  validates :link, presence: true
+
+  def convert_to_embeded
+    link.gsub!('watch?v=', 'embed/') if link.include?('watch?v=')
+  end
 end
